33d3025f8072572141d095d5934ad15667b4ea78
KantemirKuzminov <kantik1406@gmail.com.com>
2022-08-05 22:10:36 +0300

    additional changes for the functionality

--- src/js/main.js
+++ src/js/main.js
@@ -3,16 +3,23 @@ import "./slider";
 import modal from "./modules/modals";
 import {showModalByTime} from "./modules/modals";
 import forms from "./modules/forms";
+import changeModalState from "./modules/changeModalState";
+
+const modalState = {};
 
 document.addEventListener('DOMContentLoaded', () => {
     "use strict";
 
+    changeModalState(modalState);
     tabs(".glazing_slider", ".glazing_block", ".glazing_content", "active");
     tabs(".decoration_slider", ".no_click", ".decoration_content > div > div", "after_click"); 
+    tabs(".balcon_icons", ".balcon_icons_img", ".big_img > img", "do_image_more", "inline");
     showModalByTime(".popup", 5000);
-    modal(".popup_engineer", ".popup_engineer_btn");
-    modal(".popup", ".phone_link");
-    forms("form", ".popup", "input[name='user_phone']");
-    forms("form", ".popup_engineer", 'input[name="user_phone"]');
+    modal(".popup_engineer", ".popup_engineer_btn", "[data-modal]");
+    modal(".popup", ".phone_link", "[data-modal]");
+    modal(".popup_calc", ".glazing_price_btn", "[data-modal]", false);
+    modal(".popup_calc_profile", ".popup_calc_button", "[data-modal]", false);
+    modal(".popup_calc_end", ".popup_calc_profile_button", "[data-modal]", false);
+    forms("form", "[data-modal]", "input[name='user_phone']", "input[name='user_name']", modalState);
 }); 
 
--- src/js/services/services.js
+++ src/js/services/services.js
@@ -1,8 +1,8 @@
-const postData = async(url, data, contentType) => {
+const postData = async(url, data) => {
     const res = await fetch(url, {
         method: 'POST',
         /* headers: {
-            'Content-type': `application/${contentType}; charset=utf-8`
+            'Content-type': `application/json; charset=utf-8`
         }, */
         body: data
     });
@@ -11,8 +11,8 @@ const postData = async(url, data, contentType) => {
         throw new Error(`Could not fetch ${url}, status: ${res.status}`);
     }
 
+    //return await res.json(); //server.php нам отдает данные в формате текста, а не json. 
     return await res.text();
-    /* return await res.json(); */
 };
 
 export default postData;
